fafc29cf602758f522b7d9848fc30c6855d649e5
minhaics <congminhcs.ai@gmail.com>
2025-01-18 16:23:18 +0700

    modify databases variable

--- core_apps/user_auth/apps.py
+++ core_apps/user_auth/apps.py
@@ -3,5 +3,5 @@ from django.utils.translation import gettext_lazy as _
 
 class UserAuthConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "user_auth"
+    name = "core_apps.user_auth"
     verbose_name = _("User Authentication")
